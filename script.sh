09f8bbb0c0b27e194e63b14da462d2c39643d0b5
Ronaldo Luiz <ronaldoluiz747@gmail.com>
2022-12-18 18:06:31 -0300

    first commit

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Criando deployments... "
+
+kubectl apply -f .\mysql-deployment.yml 
+kubectl apply -f .\app-deployment.yml 
+
+echo "Criando secrets... "
+kubectl apply -f .\secrets.yml 
+
+echo "Criando load balance... "
+kubectl apply -f .\load-balancer.yml 
+
+echo "Criando Persistent Volume... "
+kubectl apply -f .\pv.yml 
